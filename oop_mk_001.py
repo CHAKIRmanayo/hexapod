406eaee8417d18a60766fcd905576e2cd30899b9
P0L0SKUN-EN0T0V <vovaromanov0001@gmail.com>
2024-11-29 21:39:13 +0300

    Update oop_mk_001.py
    v0.0.3

--- oop_mk_001.py
+++ oop_mk_001.py
@@ -36,8 +36,8 @@ class Meha:
     def changeBasis(fi):
         pass
     
-    def addLeg(self, key, pos, angle):
-        self.legs[key] = LEG(self, pos, angle)
+    def addLeg(self, key, joint_start, angles_leg):
+        self.legs[key] = LEG(self, joint_start, angles_leg)
 
     def getStatus(self):
         offs = 10 * ' '
@@ -55,42 +55,42 @@ class Meha:
         clearConsole()
         self.getStatus()
 
-    def draw(self):
+    def draw(self, isAnimate = True):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-
         lines = []
-        for _ in range(6):
+        for _ in range(7):
             line, = ax.plot([], [], [], 'o-', lw=2)
             lines.append(line)
 
         def update(frame):  
-            t = (time.time() - start_time)
-            ax.set_xlim(-10+t, 10+t)
-            ax.set_ylim(-10, 10)
+            t = isAnimate*(time.time() - start_time)
+            posOld = self.position 
+            self.position = 0.8*vec3(np.cos(t)-1, np.sin(t), 0)
+            dS = self.position - posOld
+            ax.set_xlim(-10+dS[0], 10+dS[0])
+            ax.set_ylim(-10+dS[1], 10+dS[1])
             ax.set_zlim(-10, 10)
             ax.set_xlabel("X")
             ax.set_ylabel("Y")
             ax.set_zlabel("Z")
-            #self.position = vec3(t, 0,0)
+            self.updateStatus()
             x0,y0,z0 = self.position
-            ax.plot([x0], [y0], [z0], 'ro')
+            lines[0].set_data([x0], [y0])
+            lines[0].set_3d_properties([z0])
 
-            self.updateStatus()     
             for i, id in enumerate(self.legs):
                 leg = self.legs[id]
-                leg.animate(t)
-                
+                leg.move(dS)
                 points = {'x': [],'y': [],'z': []}
-                
-                for jointId in leg.config['JOINTS']:
-                    x,y,z = leg.config['JOINTS'][jointId]
+                for joint in leg.JOINTS:
+                    x,y,z = joint+self.position
                     points['x'].append(x)
                     points['y'].append(y)
                     points['z'].append(z)
 
-                lines[i].set_data(points['x'], points['y'])
-                lines[i].set_3d_properties(points['z'])
+                lines[i+1].set_data(points['x'], points['y'])
+                lines[i+1].set_3d_properties(points['z'])
                 
             return lines
             
@@ -100,81 +100,89 @@ class Meha:
 
 
 class LEG():
-    def __init__(self, meha, pos, angle):
+    def __init__(self, meha, pos, angles):
         self.meha = meha
         self.pos = pos
-        self.BaseT10 = np.linalg.inv(rotationZ(angle) @ rotationX(np.pi))
+        self.isLeft = self.pos[1]>0
+        self.BaseT10 = np.linalg.inv(rotationZ(self.isLeft*np.pi) @ rotationX(np.pi))
         self.BaseT21 = np.linalg.inv(rotationX(-np.pi/2) @ rotationZ(-np.pi/2))
         self.BaseT32 = np.eye(3)
-        self.shoulders = [vec3(0,0,2), vec3(3,0,0), vec3(3,0,0)]
-        self.setup()
-        self.rotate_All_Sustavs(0,0,0)
-        self.calculate_Joints_Relative_Meha()
+        self.lenBonds = [2,3,3]
+        self.vecBonds = [vec3(0,0,self.lenBonds[0]), 
+                         vec3(self.lenBonds[1],0,0), 
+                         vec3(self.lenBonds[2],0,0)]
+        self.setupAttributes()
+        self.setAngles(*angles)
+        self.calcJoints()
+        # print(self.JOINTS)
 
-    def setup(self):
+    def setupAttributes(self):
         self.config = { 
                 'JOINTS':                {'J0': 0,'J1': 0,'J2': 0,'J3': 0,},
-                'JOINT-VELOCITYS':       {'v1': 0,'v2': 0,'v3': 0,},
-                'JOINT-ACCELERATIONS':   {'a1': 0,'a2': 0,'a3': 0,},
+                'JOINT_VELOCITYS':       {'v1': 0,'v2': 0,'v3': 0,},
+                'JOINT_ACCELERATIONS':   {'a1': 0,'a2': 0,'a3': 0,},
                 'ANGLES':                {'A1': 0,'A2': 0,'A3': 0,},
-                'ANGULAR-VELOCITYS':     {'w1': 0,'w2': 0,'w3': 0,},
-                'ANGULAR-ACCELERATIONS': {'e1': 0,'e2': 0,'e3': 0,}
+                'ANGULAR_VELOCITYS':     {'w1': 0,'w2': 0,'w3': 0,},
+                'ANGULAR_ACCELERATIONS': {'e1': 0,'e2': 0,'e3': 0,}
             }
-    
-    def updateParametr(self, param, *args):
+        cnf = self.config
+        for param in cnf: self.__setattr__(param, [cnf[param][key] for key in cnf[param]])
+        
+    def updateParameter(self, param, *args):
+        self.__setattr__(param, [*args])
         for i, paramId in enumerate(self.config[param]):
             self.config[param][paramId] = args[i]
 
-    def rotate_All_Sustavs(self,a1,a2,a3):
+    def setAngles(self,a1,a2,a3):
         a1, a2, a3 = a1, a2, a3
         self.T10 = rotationZ(a1)
         self.T21 = rotationZ(a2) 
         self.T32 = rotationZ(a3)
-        self.updateParametr('ANGLES',a1,a2,a3)
+        self.updateParameter('ANGLES',a1,a2,a3)
 
-    def calculate_Joints_Relative_Meha(self):
+    def calcJoints(self):
         R1 = self.BaseT10 @ self.T10
         R2 = R1 @ self.BaseT21 @ self.T21
         R3 = R2 @ self.BaseT32 @ self.T32
-        j0 = self.pos + self.meha.position
-        j1 = j0 + R1 @ self.shoulders[0]
-        j2 = j1 + R2 @ self.shoulders[1]
-        j3 = j2 + R3 @ self.shoulders[2]
-        self.updateParametr('JOINTS',j0,j1,j2,j3)
+        v1,v2,v3 = self.vecBonds
+        j0 = self.pos
+        j1 = j0 + R1 @ v1
+        j2 = j1 + R2 @ v2
+        j3 = j2 + R3 @ v3
+        self.updateParameter('JOINTS',j0,j1,j2,j3)
     
-    def animate(self, time):
+    def move(self, dS=0, dFi=0):
         self.check_Leg_On_Earth()
-        if self.isEarth: self.moveEarth()
-        else: self.moveAir()
-        self.calculate_Joints_Relative_Meha()
+        if self.isEarth: 
+            self.moveEarth(dS, dFi)
+        else: 
+            self.moveAir(dS, dFi)
 
     def check_Leg_On_Earth(self):
         self.isEarth = True
 
-    def moveEarth(self, dS=0, dFi=0):
-        a = np.sin(time.time())
-        a1, a2, a3 = np.pi/6, -np.pi/6, -np.pi/6
-        self.rotate_All_Sustavs(a1,a2,a3)
+    def moveEarth(self, dS, dFi):
+        vec = self.BaseT10@(self.JOINTS[3]-self.JOINTS[0]-dS)
+        a1,a2,a3 = self.inverseKinematics(vec) 
+        self.setAngles(a1,a2,a3)
+        self.calcJoints()
 
-    def moveAir(self, dS=0, dFi=0):
-        a = np.sin(time.time())
-        a1, a2, a3 = np.pi/6*a, np.pi/6*a + np.pi/6,  np.pi/6*a + np.pi/6
-        self.rotate_All_Sustavs(a1,a2,a3)
+    def moveAir(self, dS, dFi):
+        a1,a2,a3 = self.inverseKinematics(self.config['JOINTS']['J0'], self.config['JOINTS']['J3']+dS) 
+        self.setAngles(a1,a2,a3)
+        self.calcJoints()
     
-    def inverseKinematics(self, p_start, p_end):
-        s1, s2, s3 = 2, 3, 3 #переделать
-        x0, y0, z0 = p_start
-        x1, y1, z1 = p_end
-
-        A1 = np.arctan2(x0-x1,y0-y1)
-        x_prime = np.sqrt((x1 - x0)**2 + (y1 - y0)**2)
-        z_prime = z0 - z1 - s1
+    def inverseKinematics(self, v):
+        s1, s2, s3 = self.lenBonds
+        A1 = np.arctan2(v[0],v[1])
+        x_prime = np.sqrt((v[0])**2 + (v[1])**2)
+        z_prime = v[2] - s1
         d = np.sqrt(x_prime**2 + z_prime**2)
         theta2_part1 = np.arctan2(x_prime, z_prime)
         theta2_part2 = np.arccos((s2**2 + d**2 - s3**2) / (2 * s2 * d))
         A2 = -theta2_part1 - theta2_part2 + np.pi / 2
         A3 = -np.arccos((s3**2 + s2**2 - d**2) / (2 * s3 * s2)) + np.pi
-        self.updateParametr('ANGLES', A1, A2, A3)
+        return A1,A2,A3
 
     
 R_Angle = 0
@@ -183,16 +191,16 @@ pos = np.array([0,0,0])
 basis = np.eye(3)
 
 meha = Meha(pos, basis)
-meha.addLeg('R1',[3, -1, 0],   R_Angle)
-meha.addLeg('R2',[0, -2, 0],   R_Angle)
-meha.addLeg('R3',[-3, -1, 0],  R_Angle)
-meha.addLeg('L1',[3, 1, 0],  L_Angle)
-meha.addLeg('L2',[0, 2, 0],  L_Angle)
-meha.addLeg('L3',[-3, 1, 0], L_Angle) 
+meha.addLeg('R1', joint_start = [3, -1, 0],  angles_leg = [-np.pi/6, -np.pi/6, np.pi/2]   )
+meha.addLeg('R2', joint_start = [0, -2, 0],  angles_leg = [0, -np.pi/6, np.pi/2],        )
+meha.addLeg('R3', joint_start = [-3, -1, 0], angles_leg = [np.pi/6, -np.pi/6, np.pi/2]  )
+meha.addLeg('L1', joint_start = [3, 1, 0],   angles_leg = [np.pi/6, -np.pi/6, np.pi/2]  )
+meha.addLeg('L2', joint_start = [0, 2, 0],   angles_leg = [0, -np.pi/6, np.pi/2],        )
+meha.addLeg('L3', joint_start = [-3, 1, 0],  angles_leg = [-np.pi/6, -np.pi/6, np.pi/2 ]  ) 
 
 
-meha.draw()
-meha.getStatus()
+meha.draw(True)
+# meha.getStatus()
 
 
 
